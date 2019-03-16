7454e4863f17ddadade4a4108e039eb9150404ae
Vitoc <nightfuryva@gmail.com>
2019-03-16 17:17:14 +0200

    Fixed print

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from datetime import  datetime
 from time import sleep
+import sys
 
 if __name__ == '__main__':
     pass
 
 while True:
     print(datetime.now())
+    sys.stdout.flush()
     sleep(30)
