4a04b22e6014fc7063dd805a117f02671ac64082
valecs97 <nightfuryva@gmail.com>
2019-05-08 18:18:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     pass
 
 while True:
-    print(str(datetime.now()) + " webhook test2")
+    print(str(datetime.now()) + " webhook test3")
     sys.stdout.flush()
     sleep(30)
